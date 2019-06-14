cf2331b07905977bd2fe9e541888af3d6eca4b52
Abhinay Kashyap <abhi01.amu@gmail.com>
2019-06-14 16:07:21 +0530

    bubble sort

---
+++ sortingAlgorithms/bubbleSort/src/Main.java
@@ -0,0 +1,30 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+
+        for(int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--){
+            for(int i =0; i<lastUnsortedIndex; i++){
+                if(intArray[i]>intArray[i+1]){
+                    swap(intArray, i, i+1);
+                }
+            }
+        }
+
+        for (int anIntArray : intArray) {
+            System.out.println(anIntArray);
+        }
+
+    }
+
+    public static void swap(int[] array, int i, int j){
+
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+
+    }
+
+}
