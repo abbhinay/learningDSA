0fd18bf604ed3513c3c4ded37c26fef9dc6b0ee7
Abhinay Kashyap <abhi01.amu@gmail.com>
2019-06-25 23:17:26 +0530

    link to jdk linked list documentation added

--- lists/doublyLinkedList/src/com/company/Main.java
+++ lists/doublyLinkedList/src/com/company/Main.java
@@ -2,6 +2,8 @@ package com.company;
 
 public class Main {
 
+    //link to official documentation in java jdk: https://docs.oracle.com/en/java/javase/12/docs/api/java.base/java/util/LinkedList.html
+
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
         list.add(1);
